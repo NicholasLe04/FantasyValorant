bbf0da86a413cf08ab758467659585c47ab38257
SphenOz <joshua.layokun@sjsu.edu>
2022-10-12 23:47:32 -0700

    removal of str concat

--- Scripts/bot.py
+++ Scripts/bot.py
@@ -150,16 +150,15 @@ def embedPlayerInfo(player_name : str):
 def embedRosterInfo(member : Member):
     embed = discord.Embed(title=f"{member.name}'s Roster")
     #embed.add_field(name="Player 1", value=userbase.uTeamGetPlayers(str(member.id))[0], inline=False)
-    embed.add_field(name="Player 1", value=userbase.uTeamGetPlayers(str(member.id))[0], inline=False)
+    embed.add_field(name="Player 1", value=userbase.uTeamGetPlayers(member.id)[0], inline=False)
     print(id)
     print(userbase.uTeamGetPlayers(id)[0])
     print(userbase.uTeamGetPlayers)
-    embed.add_field(name="Player 2", value=userbase.uTeamGetPlayers(str(member.id))[1], inline=False)
-    embed.add_field(name="Player 3", value=userbase.uTeamGetPlayers(str(member.id))[2], inline=False)
-    embed.add_field(name="Player 4", value=userbase.uTeamGetPlayers(str(member.id))[3], inline=False)
-    embed.add_field(name="Player 5", value=userbase.uTeamGetPlayers(str(member.id))[4], inline=False)
-    print(userbase.uTeamGetPlayers(str(member.id))[4])
-    print(str(member.id))
+    embed.add_field(name="Player 2", value=userbase.uTeamGetPlayers(member.id)[1], inline=False)
+    embed.add_field(name="Player 3", value=userbase.uTeamGetPlayers(member.id)[2], inline=False)
+    embed.add_field(name="Player 4", value=userbase.uTeamGetPlayers(member.id)[3], inline=False)
+    embed.add_field(name="Player 5", value=userbase.uTeamGetPlayers(member.id)[4], inline=False)
+    print(userbase.uTeamGetPlayers(member.id)[4])
     return (embed)
 
 ### Getter Methods                                                                                                          ***TO BE ADDED TO DB.PY***
